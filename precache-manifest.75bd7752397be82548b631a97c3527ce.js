84d6ed46c20c8caf593322f04af737fb07877093
yoadfekete <yoadfekete@microsoft.com>
2022-01-15 11:53:11 +0200

    Updates

---
+++ precache-manifest.75bd7752397be82548b631a97c3527ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff308fc13393afb7ddee5a134cbf418f",
+    "url": "/lifshitz-generator/index.html"
+  },
+  {
+    "revision": "aafdcf758e62a8bfbaf3",
+    "url": "/lifshitz-generator/static/css/2.c1c45ef1.chunk.css"
+  },
+  {
+    "revision": "aafdcf758e62a8bfbaf3",
+    "url": "/lifshitz-generator/static/js/2.c9c9a1cf.chunk.js"
+  },
+  {
+    "revision": "ef98b4a934e4c8d0ecb9",
+    "url": "/lifshitz-generator/static/js/main.d564e613.chunk.js"
+  },
+  {
+    "revision": "6287b68ef48478da0eae",
+    "url": "/lifshitz-generator/static/js/runtime-main.fcc627d3.js"
+  }
+]);
